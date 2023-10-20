2346b81027e8f7395e3f7c491aa4dbdf998a0d13
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-10-19 22:22:36 -0400

    adds database in connection and .env file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
+require('dotenv').config(); // Load environment variables from .env file
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern_book_search');
 
 module.exports = mongoose.connection;
+
+
+/////
+
